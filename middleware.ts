3cdbd1d6895d640f2bc47119c126426cb9a6092c
coolkid!playcode <161105950+CamilaTran@users.noreply.github.com>
2025-04-24 11:15:45 +0700

    setup eslint + prettier

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+
+export function middleware(request: NextRequest) {
+    // console.log(request)
+    return NextResponse.next();
+}
