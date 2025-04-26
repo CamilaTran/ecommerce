8276e78788d4d6092c2b64f28da4412b10095390
coolkid!playcode <161105950+CamilaTran@users.noreply.github.com>
2025-04-26 10:02:11 +0700

    info cmp

--- components/products/info/index.tsx
+++ components/products/info/index.tsx
@@ -1,27 +1,36 @@
-import { Product } from "@/types/product.types";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Product } from '@/types/product.types';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
-import './styles.scss'
+import './styles.scss';
 
 interface InfoProps {
-  info: Product["info"]
+  info: Product['info'];
 }
 
 const Info = ({ info }: InfoProps) => {
-  return <>
-    <Accordion type="multiple" className="w-full">
-      {info.map(({ title, description }, index) => <AccordionItem value={title} key={index}>
-        <AccordionTrigger>
-          {title}
-        </AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            {description.map((i, idx) => <li key={idx}>{i}</li>)}
-          </ul>
-        </AccordionContent>
-      </AccordionItem>)}
-    </Accordion>
-  </>;
+  return (
+    <>
+      <Accordion type="multiple" className="w-full">
+        {info.map(({ title, description }, index) => (
+          <AccordionItem value={title} key={index}>
+            <AccordionTrigger>{title}</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                {description.map((i, idx) => (
+                  <li key={idx}>{i}</li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
 };
 
 export default Info;
--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,16 +1,16 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import * as React from 'react';
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
 
-import { cn } from "@/lib/utils"
-import ExpandIcon from "../icons/expand-icon"
-import CollapseIcon from "../icons/collapse-icon"
+import { cn } from '@/lib/utils';
+import ExpandIcon from '../icons/expand-icon';
+import CollapseIcon from '../icons/collapse-icon';
 
 function Accordion({
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
-  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
 }
 
 function AccordionItem({
@@ -20,10 +20,13 @@ function AccordionItem({
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0 border-neutral-200 border-b-1", className)}
+      className={cn(
+        'border-b last:border-b-0 border-neutral-200 border-b-1',
+        className
+      )}
       {...props}
     />
-  )
+  );
 }
 
 function AccordionTrigger({
@@ -36,7 +39,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "!text-neutral-900 font-medium text-lg focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md pt-8 pb-6 text-left transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg:first-of-type]:hidden  [&[data-state=closed]>svg:last-of-type]:hidden",
+          '!text-neutral-900 font-medium text-lg focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md pt-8 pb-6 text-left transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg:first-of-type]:hidden  [&[data-state=closed]>svg:last-of-type]:hidden',
           className
         )}
         {...props}
@@ -44,11 +47,9 @@ function AccordionTrigger({
         {children}
         <CollapseIcon />
         <ExpandIcon />
-
-       
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  )
+  );
 }
 
 function AccordionContent({
@@ -62,9 +63,9 @@ function AccordionContent({
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-base text-neutral-600"
       {...props}
     >
-      <div className={cn("pt-0 pb-8", className)}>{children}</div>
+      <div className={cn('pt-0 pb-8', className)}>{children}</div>
     </AccordionPrimitive.Content>
-  )
+  );
 }
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
